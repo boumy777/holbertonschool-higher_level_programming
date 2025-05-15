9c987370d9222c9bbd0622a164158829ec851f77
root <brandondialuta@gmail.com>
2025-05-16 01:09:22 +0200

    function that divides

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,13 +2,17 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
+        division = 0
         try:
-            val1 = my_list_1[i]
-            val2 = my_list_2[i]
-            res = val1 / val2
-        except TypeError:
-            print("wrong type")
-            res = 0
+            num1 = my_list_1[i]
+            num2 = my_list_2[i]
+            division = num1 / num2
         except ZeroDivisionError:
             print("division by 0")
-            res = 0
+        except TypeError:
+            print("wrong type")
+        except IndexError:
+            print("out of range")
+        finally:
+            result.append(division)
+    return result
