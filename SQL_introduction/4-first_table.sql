e02bb613ebcde407cdd2286af3dc19ea34c08b85
boumy777 <brandondialuta@gmail.com>
2025-06-27 16:09:44 +0200

    script that creates

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Create a table called first_table if it does not exist
+-- first_table description: id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
