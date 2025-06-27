9a5a50453576b6dab112b2ee22785f6a9595ee66
boumy777 <brandondialuta@gmail.com>
2025-06-27 16:25:57 +0200

    script that lists all record

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Sélectionne les colonnes 'score' et 'name' de la table second_table
+SELECT score, name
+FROM second_table
+-- Trie les résultats par score en ordre décroissant (du plus haut au plus bas)
+ORDER BY score DESC;
